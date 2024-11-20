776fd1f1d1f162675761f85cb67fbea2a11a61f6
Mayuradlak212 <156865950+Mayuradlak212@users.noreply.github.com>
2024-11-20 19:59:27 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+function Home(){
+    return (
+        <div>
+            <h1>Welcome to My Homepage</h1>
+            <p>This is a simple React App</p>
+            <p>This is a paragraph</p>
+            <div>
+                <div>
+                    Nth Tag 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
